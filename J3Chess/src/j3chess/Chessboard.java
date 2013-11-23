0cbc08f2be37a3f6bcdd8d012ed8e270b8fdeb36
derHai <derhai@hettschach.de>
2013-11-23 11:43:40 +0100

    Create chessboard create Fields with all the Neighbors

--- J3Chess/src/j3chess/Chessboard.java
+++ J3Chess/src/j3chess/Chessboard.java
@@ -8,32 +8,122 @@ import j3chess.utility.Vector2d;
  */
 public class Chessboard {
 
+	public final int NUMBEROFCIRCELS=6;
+	public final int NUMBEROFCOLUMNS=24; 
+	/** @brief 6x24 array of all fields in the game */
+	private Field mFields[][];
+	
+	
 	/**
 	 * @brief Creates new instance of the Chessboard class.
 	 */
 	public Chessboard() {
 		// Create all logical fields
 		createFields();
+		//set the Neighbors of every Field
+		setNeighbors();
+		//print all Fields - just for debugging
+		printAllFields();
 		
 		// Calculate the fields' 2D drawing positions
 		calculateDrawPositions(0.9f);
 	}
+	private void printAllFields(){
+		for(int circles = 0; circles < NUMBEROFCIRCELS; circles++) {
+			for(int columns = 0; columns < NUMBEROFCOLUMNS; columns++) {
+				mFields[columns][circles].print();
+			}
+		}		
+	}
 	
+	private void setNeighbors(){
+		for(int circle = 0; circle < NUMBEROFCIRCELS; circle++) {
+			for(int column = 0; column < NUMBEROFCOLUMNS; column++) {
+				Field field=mFields[column][circle];
+				setInner(field,column,circle);
+				setOuter(field,column,circle);
+				setLeft(field,column,circle);
+				setRight(field,column,circle);
+				setLeftInner(field,column,circle);
+				setLeftOuter(field,column,circle);
+				setRightInner(field,column,circle);
+				setRightOuter(field,column,circle);
+			}
+		}		
+	}
+	
+	
+	private void setRight(Field field, int column, int circle) {
+		field.setRightNeighbor(mFields[((column+1)%NUMBEROFCOLUMNS)][circle]);	
+	}
+	
+	private void setLeft(Field field, int column, int circle) {
+		if(column==0){
+			field.setLeftNeighbor(mFields[NUMBEROFCOLUMNS-1][circle]);
+		}else{
+			field.setLeftNeighbor(mFields[column-1][circle]);
+		}
+	}
+	
+	private void setInner(Field field, int column, int circle) {
+		if(circle<NUMBEROFCIRCELS-1){
+			field.setInnerNeighbor(mFields[column][circle+1]);
+		}else
+			field.setInnerNeighbor(mFields[((column+NUMBEROFCOLUMNS/2)%NUMBEROFCOLUMNS)][circle]);
+	}
+	
+	private void setOuter(Field field, int column, int circle) {
+		if(circle>0){
+			field.setOuterNeighbor(mFields[column][circle-1]);
+		}
+	}
+	
+	private void setRightOuter(Field field, int column, int circle) {
+		if(circle>0){
+			field.setRightOuterNeighbor(mFields[(column+1)%NUMBEROFCOLUMNS][circle-1]);
+		}
+	}
+	
+	private void setLeftOuter(Field field, int column, int circle) {
+		if(circle>0){
+			if(column>0){
+				field.setLeftOuterNeighbor(mFields[column-1][circle-1]);
+			}else{
+				field.setLeftOuterNeighbor(mFields[NUMBEROFCOLUMNS-1][circle-1]);	
+			}
+		}
+	}
+	
+	private void setRightInner(Field field, int column, int circle) {
+		if(circle<NUMBEROFCIRCELS-1){
+			field.setRightInnerNeighbor(mFields[(column+1)%NUMBEROFCOLUMNS][circle+1]);
+		}else{
+			//diagonal through the middle
+			field.setRightInnerNeighbor(mFields[(column+((NUMBEROFCIRCELS-1)*2))%NUMBEROFCOLUMNS][circle]);
+		}
+	}
+	
+	private void setLeftInner(Field field, int column, int circle) {
+		if(circle<NUMBEROFCIRCELS-1){
+			if(column!=0){
+				field.setLeftInnerNeighbor(mFields[column-1][circle+1]);
+			}else
+				field.setLeftInnerNeighbor(mFields[NUMBEROFCOLUMNS-1][circle+1]);
+		}else{
+			//diagonal through the middle
+			field.setLeftInnerNeighbor(mFields[(column+(NUMBEROFCOLUMNS-((NUMBEROFCIRCELS-1)*2)))%NUMBEROFCOLUMNS][circle]);
+		}
+	}
 	/**
 	 * @brief Creates all the fields and adds the required connections.
 	 */
 	private void createFields() {
-		// Create field array
-		mFields = new Field[6][24];
-		
-		// TODO remove and replace with Kai's implementation
-		for (int m=0; m<6; m++) {
-			for (int n=0; n<24; n++) {
-				mFields[m][n] = new Field();
+		mFields=new Field[NUMBEROFCOLUMNS][NUMBEROFCIRCELS];
+		for(int i = 0; i < NUMBEROFCOLUMNS; i++) {
+			for(int j = 0; j < NUMBEROFCIRCELS; j++) {
+				mFields[i][j] = new Field(j,i);
 			}
 		}
-		
-		// TODO #10 Add connections to fields
 	}
 	
 	/**
@@ -63,7 +153,7 @@ public class Chessboard {
 				Vector2d cartesian = Vector2d.fromPolarCoordinates(r, rho);
 				
 				// Set coordinates of field
-				mFields[m][n].setDrawPosition(cartesian);
+				mFields[n][m].setDrawPosition(cartesian);
 			}
 		}
 	}
@@ -85,7 +175,5 @@ public class Chessboard {
 		return null;
 	}
 	
-	/** @brief 6x24 array of all fields in the game */
-	private Field mFields[][];
 	
 }
--- J3Chess/src/j3chess/Field.java
+++ J3Chess/src/j3chess/Field.java
@@ -2,100 +2,126 @@ package j3chess;
 
 import j3chess.utility.Vector2d;
 
+
+
 public class Field {
 
+	/** @brief Position where the field is drawn */
+	private Vector2d mDrawPos;
+
+	private Field leftNeighbor;
+	private Field rightNeighbor;
+	private Field innerNeighbor;
+	private Field outerNeighbor;
+	
+	private Field leftInnerNeighbor;
+	private Field rightInnerNeighbor;
+	private Field leftOuterNeighbor;
+	private Field rightOuterNeighbor;
+	
+	public Integer circle;
+	public int column;
+	public char columnNames[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X'};
+
+	
+	
 	/** @brief Creates a field of the chessboard */
-	public Field() {
+	public Field(int circle, int column) {
 		mDrawPos = new Vector2d();
-	}
-	
-	/** @brief Sets the left inner neighbor of this field. */
-	public void setLeftInnerNeighbor(Field f) {
-		// TODO implement
-	}
-	
-	/** @brief Returns the left inner neighbor of this field. */
-	public Field getLeftInnerNeighbor() {
-		// TODO implement
-		return null;
-	}
-	
-	/** @brief Sets the inner neighbor of this field. */
-	public void setInnerNeighbor(Field f) {
-		// TODO implement
-	}
-	
-	/** @brief Returns the inner neighbor of this field. */
-	public Field getInnerNeighbor() {
-		// TODO implement
-		return null;
-	}
-	
-	/** @brief Sets the right inner neighbor of this field. */
-	public void setRightInnerNeighbor(Field f) {
-		// TODO implement
-	}
-	
-	/** @brief Returns the right inner neighbor of this field. */
-	public Field getRightInnerNeighbor() {
-		// TODO implement
-		return null;
-	}
-	
-	/** @brief Sets the left neighbor of this field. */
-	public void setLeftNeighbor(Field f) {
-		// TODO implement
+		this.circle=circle;
+		this.column=column;
 	}
 	
 	/** @brief Returns the left neighbor of this field. */
 	public Field getLeftNeighbor() {
-		// TODO implement
-		return null;
+		return leftNeighbor;
 	}
+  
 	
-	/** @brief Sets the right neighbor of this field. */
-	public void setRightNeighbor(Field f) {
-		// TODO implement
+	/** @brief Sets the left neighbor of this field. */
+	public void setLeftNeighbor(Field leftNeighbor) {
+		this.leftNeighbor = leftNeighbor;
 	}
-	
+
 	/** @brief Returns the right neighbor of this field. */
 	public Field getRightNeighbor() {
-		// TODO implement
-		return null;
+		return rightNeighbor;
 	}
-	
-	/** @brief Sets the left outer neighbor of this field. */
-	public void setLeftOuterNeighbor(Field f) {
-		// TODO implement
+	/** @brief Sets the right neighbor of this field. */
+	public void setRightNeighbor(Field rightNeighbor) {
+		this.rightNeighbor = rightNeighbor;
 	}
-	
-	/** @brief Returns the left outer neighbor of this field. */
-	public Field getLeftOuterNeighbor() {
-		// TODO implement
-		return null;
+	/** @brief Returns the inner neighbor of this field. */
+	public Field getInnerNeighbor() {
+		return innerNeighbor;
 	}
-	
-	/** @brief Sets the outer neighbor of this field. */
-	public void setOuterNeighbor(Field f) {
-		// TODO implement
+
+	/** @brief Sets the inner neighbor of this field. */
+	public void setInnerNeighbor(Field innerNeighbor) {
+		this.innerNeighbor = innerNeighbor;
 	}
-	
 	/** @brief Returns the outer neighbor of this field. */
 	public Field getOuterNeighbor() {
-		// TODO implement
-		return null;
+		return outerNeighbor;
 	}
-	
-	/** @brief Sets the right outer neighbor of this field. */
-	public void setRightOuterNeighbor(Field f) {
-		// TODO implement
+	/** @brief Sets the outer neighbor of this field. */
+	public void setOuterNeighbor(Field outerNeighbor) {
+		this.outerNeighbor = outerNeighbor;
 	}
-	
+
+	/** @brief Returns the left inner neighbor of this field. */
+	public Field getLeftInnerNeighbor() {
+		return leftInnerNeighbor;
+	}
+
+	/** @brief Sets the left inner neighbor of this field. */
+	public void setLeftInnerNeighbor(Field leftInnerNeighbor) {
+		this.leftInnerNeighbor = leftInnerNeighbor;
+	}
+	/** @brief Returns the right inner neighbor of this field. */
+	public Field getRightInnerNeighbor() {
+		return rightInnerNeighbor;
+	}
+	/** @brief Sets the right inner neighbor of this field. */
+	public void setRightInnerNeighbor(Field rightInnerNeighbor) {
+		this.rightInnerNeighbor = rightInnerNeighbor;
+	}
+	/** @brief Returns the left outer neighbor of this field. */
+	public Field getLeftOuterNeighbor() {
+		return leftOuterNeighbor;
+	}
+
+	/** @brief Sets the left outer neighbor of this field. */
+	public void setLeftOuterNeighbor(Field leftOuterNeighbor) {
+		this.leftOuterNeighbor = leftOuterNeighbor;
+	}
 	/** @brief Returns the right outer neighbor of this field. */
 	public Field getRightOuterNeighbor() {
-		// TODO implement
-		return null;
+		return rightOuterNeighbor;
 	}
+	/** @brief Sets the right outer neighbor of this field. */
+	public void setRightOuterNeighbor(Field rightOuterNeighbor) {
+		this.rightOuterNeighbor = rightOuterNeighbor;
+	}
+
+	/** @brief prints the field with his Neighbors - just for debugging */
+	public void print(){
+		System.out.println(this.toString() + 
+				" left:" + leftNeighbor + 
+				" right:" + rightNeighbor + 
+				" inner:" + innerNeighbor + 
+				" Outer:" + outerNeighbor + 
+				" LeftOuter:" + leftOuterNeighbor + 
+				" RightOuter:" + rightOuterNeighbor + 
+				" LeftInner:" + leftInnerNeighbor + 
+				" RightInner:" + rightInnerNeighbor);
+	}	
+
+	
+	public String toString(){
+		return columnNames[column] + circle.toString();
+	}	
+
 	
 	/**
 	 * @brief Sets the position where the field (i.e. its piece) will be drawn.
@@ -106,17 +132,6 @@ public class Field {
 		mDrawPos.y = vec.y;
 	}
 	
-	/** @brief Position where the field is drawn */
-	private Vector2d mDrawPos;
-	
-	// TODO Uncomment when Entity class exists
-	/*private Entity mNeighborLeftInner;
-	private Entity mNeighborInner;
-	private Entity mNeighborRightInner;
-	private Entity mNeighborLeft;
-	private Entity mNeighborRight;
-	private Entity mNeighborLeftOuter;
-	private Entity mNeighborOuter;
-	private Entity mNeighborRightOuter;*/
+
 	
 }
