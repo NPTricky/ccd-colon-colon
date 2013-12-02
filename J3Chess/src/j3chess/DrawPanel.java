06783b8fac47c8757da3df2889ee4cb6dffc5c4b
David Kuri <info@davidkuri.de>
2013-12-02 18:12:14 +0100

    Added not-so-pretty scaling to DrawPanel

--- J3Chess/src/j3chess/DrawPanel.java
+++ J3Chess/src/j3chess/DrawPanel.java
@@ -8,24 +8,20 @@ import java.awt.Image;
 import javax.swing.JComponent;
 
 
-
 class DrawPanel extends JComponent {
 
-    Image img;
+    private Image mImg;
 
-
-    
     public DrawPanel(Image img) {
-    	this.img=img;
-    	Dimension dm = new Dimension(img.getWidth(null), img.getHeight(null));
+        mImg = img;
+        Dimension dm = new Dimension(img.getWidth(null), img.getHeight(null));
         setPreferredSize(dm);
     }
-    
-  
+
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
-        g2d.drawImage(img, 0, 0, null);
+        g2d.drawImage(mImg, 0, 0, this.getWidth(), this.getHeight(), null);
     }
 }
