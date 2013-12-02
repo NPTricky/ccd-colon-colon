c6b8a5421d442c0db12ceaeeaba3ad31ff6ccd9e
David Kuri <info@davidkuri.de>
2013-12-02 14:12:04 +0100

    Minor content manager fixes

--- J3Chess/src/j3chess/ContentManager.java
+++ J3Chess/src/j3chess/ContentManager.java
@@ -6,72 +6,72 @@ import java.util.HashMap;
 import javax.swing.ImageIcon;
 
 public class ContentManager {
-	
+
 	private String sourcePath;
-	private HashMap <String,ImageIcon> contant = new HashMap<String, ImageIcon>(); 
-	
-	
-	public ContentManager(String sourcePath){
-		this.sourcePath=sourcePath;
+	private HashMap<String, ImageIcon> content = new HashMap<String, ImageIcon>();
+
+
+	public ContentManager(final String sourcePath) {
+		this.sourcePath = sourcePath;
 	}
 
 
-	public ImageIcon getContant(String filename){	
-		if(contant.containsKey(filename)){
-			return contant.get(filename);
-		}else{
-			if(getContantFromFileSystem(filename, sourcePath)){
-				return contant.get(filename);
+	public final ImageIcon getContent(final String filename) {
+		if (content.containsKey(filename)) {
+			return content.get(filename);
+		} else {
+			if (getContentFromFileSystem(filename, sourcePath)) {
+				return content.get(filename);
 			}
 		}
 		//TODO image not found exception
-		return null;		
+		return null;
 	}
-	
-	
-	
-	
+
+
+
+
 	/**
 	 * @ Brief searches recursive for a file which contains the search value
 	 * @param searchFileName searched filename
 	 * @param directoryPath Path for searching the file
 	 * @return returns true if a file is founded which contains the search FileName
 	 */
-	private Boolean getContantFromFileSystem (String searchFileName, String directoryPath){
+	private Boolean getContentFromFileSystem (final String searchFileName, String directoryPath) {
 
-		if(!directoryPath.endsWith("/")){
-			directoryPath=directoryPath+"/";
+		if (!directoryPath.endsWith("/")) {
+			directoryPath = directoryPath+"/";
 		}
-		
-		Boolean returnValue=false;
-		File tmpFile =null;
-		String tmpPath=null;
-		
+
+		Boolean returnValue = false;
+		File tmpFile = null;
+		String tmpPath = null;
+
 		File directory = new File(directoryPath);
-		String [] fileNameArray =directory.list();
-		
-		
-		for(int i=0;i<fileNameArray.length;i++){
-			tmpPath=directoryPath+fileNameArray[i];
-			if(fileNameArray[i].contains(searchFileName)){
+		String [] fileNameArray = directory.list();
+
+
+		for (int i = 0; i < fileNameArray.length; i++) {
+			tmpPath = directoryPath + fileNameArray[i];
+			if (fileNameArray[i].contains(searchFileName)) {
 				//file found
 				addToHashmap(tmpPath, searchFileName);
 				return true;
 			}
-			
-			tmpFile =new File(tmpPath);	
-			if (tmpFile.isDirectory()){
-				returnValue= getContantFromFileSystem(searchFileName,(tmpPath));
+
+			tmpFile = new File(tmpPath);
+			if (tmpFile.isDirectory()) {
+				returnValue = getContentFromFileSystem(searchFileName, tmpPath);
 			}
-		
+
 		}
-		return returnValue;		
+		return returnValue;
 	}
-	
-	
-	private void addToHashmap (String path, String key){
-		ImageIcon imageIcon=new ImageIcon(path);
-		contant.put(key, imageIcon);	
+
+
+	private void addToHashmap(final String path, final String key) {
+		ImageIcon imageIcon = new ImageIcon(path);
+		content.put(key, imageIcon);
 	}
 
 }
